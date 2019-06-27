69eeec1edb580b3e065b440c796acfb62f963cfb
Arkil Thakkar <thakkararkil1996@gmail.com>
2019-06-27 00:48:03 -0700

    updated requirements

--- flaskApi.py
+++ flaskApi.py
@@ -14,12 +14,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-   return flask.render_template('index.html')   
+   return flask.render_template('/src/index.js')   
 @app.route('/predict', methods=['POST'])
 def make_prediction():
 	if request.method=='POST':
 		file = request.files['image']
-		if not file: return render_template('index.html', label="No file")
+		if not file: return render_template('/src/index.js', label="No file")
 		
 		img = misc.imread(file)
 		img = img[:,:,:3]
@@ -34,7 +34,7 @@ def make_prediction():
 		# switch for case where label=10 and number=0
 		if label=='10': label='0'
 
-		return render_template('index.html', label=label)
+		return render_template('/src/index.js', label=label)
    
 if __name__ == '__main__':
     model = joblib.load('model.pkl')
